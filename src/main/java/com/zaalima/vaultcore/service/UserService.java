f7e6e961dc93951d293d8e7d12ca94bef706dd70
Adarsh-git02 <guptaadarsh628@gmail.com>
2026-01-23 00:18:13 +0530

    Encrypt user passwords using BCrypt

--- src/main/java/com/zaalima/vaultcore/service/UserService.java
+++ src/main/java/com/zaalima/vaultcore/service/UserService.java
@@ -2,6 +2,7 @@ package com.zaalima.vaultcore.service;
 
 import com.zaalima.vaultcore.entity.User;
 import com.zaalima.vaultcore.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,14 +10,18 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final AccountService accountService;
+    private final PasswordEncoder passwordEncoder;
 
     public UserService(UserRepository userRepository,
-                       AccountService accountService) {
+                       AccountService accountService,
+                       PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.accountService = accountService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User registerUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         User savedUser = userRepository.save(user);
         accountService.createAccountForUser(savedUser);
         return savedUser;
